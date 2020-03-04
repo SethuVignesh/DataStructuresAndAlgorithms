f3fe29cdea53f3b60e1e3a7c0b649dec586476e5
Sethu Vignesh <sethu@circles.asia>
2020-03-05 02:28:59 +0800

    QUEUE implementation using LinkedList

--- src/Queue.java
+++ src/Queue.java
@@ -1,3 +1,30 @@
+import LinkedL.LL;
+import LinkedL.Node;
+
 public class Queue {
-    public void reverseAQ(){}
+    int size;
+    LL ll ;
+
+    Queue(){
+        ll= new LL();
+    }
+
+
+    public boolean enque(int data){
+        ll.insertAtPosition(data, size);
+        size++;
+        return true;
+    }
+    public int deque(){
+        Node temp =ll.head;
+
+        ll.head= temp.getNext();
+        size--;
+        return temp.getData();
+
+    }
+    public void reverseAQ(){
+        Node temp= ll.head;
+        ll.reverseLLLinear();
+    }
 }
