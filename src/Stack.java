a85441abffee19c391381d0f9ba8d9e5ef96d259
Sethu Vignesh <sethu@circles.asia>
2020-03-05 02:20:54 +0800

    Stack implementation using LinkedList

--- src/Stack.java
+++ src/Stack.java
@@ -1,5 +1,40 @@
+
+
+import LinkedL.LL;
+import LinkedL.Node;
+
+import java.util.LinkedList;
+
 public class Stack {
-//    public boolean push(){}
+    int size;
+    LL ll;
+
+    public Stack() {
+        ll = new LL();
+
+    }
+
+    public boolean push(int data) {
+        return ll.insertAtBegin(data);
+    }
+
+    public int pop() {
+        if (isEmpty()) return -1;
+        Node temp = ll.head;
+        ll.head = ll.head.getNext();
+        return temp.getData();
+    }
+
+    public int peek() {
+        if (isEmpty()) return -1;
+
+        return ll.head.getData();
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
 //    public int pop(){}
 //    public int peek(){}
 }
+
