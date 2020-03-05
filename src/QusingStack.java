7cc08d4583ce78197685013f8308a400f858e053
Sethu Vignesh <sethu@circles.asia>
2020-03-05 13:54:23 +0800

    QUEUE using Stack

--- src/QusingStack.java
+++ src/QusingStack.java
@@ -1,2 +1,24 @@
 public class QusingStack {
+    Stack stack = new Stack();
+    Stack stack2 = new Stack();
+    int size= stack.size+stack2.size;
+
+    public boolean enque(int data) {
+        stack.push(data);
+        return true;
+    }
+
+    public int deque() {
+        if (stack2.isEmpty() == false) {
+            return stack2.pop();
+        } else {
+            while (stack.isEmpty()) {
+                stack2.push(stack.pop());
+            }
+            return stack2.pop();
+        }
+    }
+
+
+
 }
