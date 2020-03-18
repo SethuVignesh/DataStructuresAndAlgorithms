4ab0b360404677c0987b67a9e48944bfdeb3cd8a
Sethu Vignesh <sethu@circles.asia>
2020-03-18 14:46:10 +0800

    Bubble sort

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -1,4 +1,28 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort {
+
+    public void bubbleSort(int[] data) {
+
+        System.out.print(Arrays.toString(data));
+
+        for (int i = 0; i < data.length-1; i++) {
+            for (int j = i+1; j < data.length; j++) {
+
+                if (data[i] > data[j]) {
+                    swap(data, i, j);
+                }
+            }
+        }
+        System.out.print(Arrays.toString(data));
+    }
+
+    private void swap(int[] data, int iPos, int jPos) {
+        int k = data[iPos];
+        data[iPos] = data[jPos];
+        data[jPos] = k;
+    }
+
 }
---
+++ src/Sorting/Solution.java
@@ -0,0 +1,9 @@
+package Sorting;
+
+public class Solution {
+    public static void main(String[] args) {
+        int data[] = new int[]{7, 6, 54, 3, 2, 2};
+        BubbleSort bubbleSort= new BubbleSort();
+        bubbleSort.bubbleSort(data);
+    }
+}
