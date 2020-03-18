94fea0dc57689d04f00ed80587f889be1638fe2a
Sethu Vignesh <sethu@circles.asia>
2020-03-18 18:18:51 +0800

    Bubble sort Insertion sort

--- src/Sorting/SelectionSort.java
+++ src/Sorting/SelectionSort.java
@@ -1,4 +1,28 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class SelectionSort {
+    public void sort(int[] data) {
+        System.out.print(Arrays.toString(data));
+
+        for (int j = 0; j < data.length; j++) {
+            int min = data[j];
+            int minPos = j;
+            for (int i = j; i < data.length; i++) {
+                if (data[i] < min) {
+                    min = data[i];
+                    minPos = i;
+                }
+            }
+            int temp = data[j];
+            data[j] = min;
+            data[minPos] = temp;
+
+
+        }
+
+        System.out.print(Arrays.toString(data));
+
+    }
 }
--- src/Sorting/Solution.java
+++ src/Sorting/Solution.java
@@ -5,7 +5,10 @@ public class Solution {
         int data[] = new int[]{7, 6, 54, 3, 2, 2};
 //        BubbleSort bubbleSort= new BubbleSort();
 //        bubbleSort.bubbleSort(data);
-        InsertionSort insertionSort= new InsertionSort();
-        insertionSort.sort(data);
+//        InsertionSort insertionSort= new InsertionSort();
+//        insertionSort.sort(data);
+
+        SelectionSort selectionSort= new SelectionSort();
+        selectionSort.sort(data);
     }
 }
